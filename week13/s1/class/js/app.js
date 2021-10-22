d7a885de512f1a2717124d6c939f1d500fc5df9d
sepehr <sepehriran3@gmail.com>
2021-10-22 10:50:43 +0330

    sepe

---
+++ week13/s1/class/js/app.js
@@ -0,0 +1,35 @@
+// var secret = "sepehr1389";
+// var userpass;
+// while(userpass != secret) {
+//  userpass = prompt("enter password")
+// }
+// var x = 0;
+// while(x < 101) {
+//     document.write(x + "<br>")
+//         x++
+
+// }
+
+function writesepehr () {
+    console.log("hello")
+}
+writesepehr();
+
+function sepehr1389(num1 , num2) {
+   console.log(num1 + num2)
+}
+
+sepehr1389(15 , 20)
+
+function sepehr19(num1 , num2) {
+    console.log(num1 - num2)
+ }
+
+ sepehr19(19 , 9)
+
+ function sepehr() {
+     return "sepehr"
+ }
+
+ var myName = sepehr();
+ console.log(myName)
---
+++ week13/s1/project/js/app.js
@@ -0,0 +1,14 @@
+function num () {
+    var number = parseInt(prompt("enter number"))
+    for (let i = 0; i <= number; i++){
+        document.write(i + "<br>")
+        }
+}
+
+num()
+function factor () {
+    var numi = parseInt(prompt("enter number"))
+    console.log(numi *5*4*3*2*1)
+}
+
+factor()
